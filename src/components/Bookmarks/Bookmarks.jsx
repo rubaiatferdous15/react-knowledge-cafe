4abaca100cf6bd9fe6393edd02f4eba1f83b2fd9
Rubaiat15 <rubaiatferdous15@gmail.com>
2024-10-22 15:57:16 +0600

    joy

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -5,7 +5,7 @@ const Bookmarks = ({bookmarks, readingTime}) => {
     return (
         <div className="md:w-1/3 bg-gray-300 ml-4 mt-2 pt-4">
            <div>
-                <h3 className="text-4xl">Reading Time:{readingTime}</h3>
+                <h3 className="text-4xl">Reading Times:{readingTime}</h3>
            </div>
            
             <h2 className="text-3xl text-center ">Bookmarks: {bookmarks.length}</h2>
